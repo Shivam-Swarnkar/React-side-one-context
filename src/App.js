b615c633a1d7e1afc741e73db62010ac025c11ac
Shivam Swarnkar <hi.swarnkar7549@gmail.com>
2021-05-27 13:29:19 +0530

    react side one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import context from "./context";
+import Context from "./context";
+import Provider from "./provider";
 
-function App() {
+const Agent = () => {
+  return <AgentOne />
+}
+
+const AgentOne = () => {
+  return <AgentTwo />
+}
+
+const AgentTwo = () => {
+  return <AgentBond />
+}
+
+const AgentBond = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Context.Consumer>
+      {context => (
+        <Fragment>
+          <p>Mission name: {context.data.mname}</p>
+          <p>Mission status: {context.data.accept}</p>
+          <button onClick={context.isMisssionAccepted}>Choose to accept</button>
+        </Fragment>
+      )
+      }
+    </Context.Consumer>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mission details</h1>
+      <Provider>
+        <Agent />
+      </Provider>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/problem.js
@@ -0,0 +1,27 @@
+// import React, {useState} from "react";
+
+// const GrandChild = props => 
+// (
+//   <div>
+//   <h3>Grandchild</h3>
+//   <Child brand={props.brand}/>
+//   </div>
+// )
+// const Child = props => 
+// (
+//   <div>
+//     <h2>Child : {props.brand}</h2>
+//   </div>
+// )
+
+// const App = () => {
+// const [brandname] = useState("Amazon");
+// return (
+// <div>
+//   <h1>Parent</h1>
+//   <GrandChild brand={brandname}/>
+// </div>
+// )
+// }
+
+// export default App;
